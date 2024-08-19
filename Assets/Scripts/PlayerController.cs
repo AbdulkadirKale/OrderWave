3087647282392480e6f4123b4630d0bb157e7566
Abdulkadir Kale <kadir.kale55@gmail.com>
2024-08-19 21:17:07 +0300

    The initial object, a table, was incorporated into the system and interaction methods were devised for it. Enhancements were then made to the input system to enable interaction with the object using the letter 'E'.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,12 +7,52 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 7f;
     [SerializeField] GameInput gameInput;
+    [SerializeField] LayerMask countersLayerMask;
 
     private bool isWalking;
+    private Vector3 lastInteractDir;
+
+    private void Start()
+    {
+        gameInput.OnInteractAction += GameInput_OnInteractAction;
+    }
+
+    private void GameInput_OnInteractAction(object sender, EventArgs e)
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        if (moveDir != Vector3.zero)
+        {
+            lastInteractDir = moveDir;
+        }
+
+        float interactDistance = 2f;
+        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask))
+        {
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                clearCounter.Interact();
+            }
+        }
+    }
 
     private void Update()
     {
+        HandleInteractions();
+        HandleMovement();
+    }
 
+
+
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+
+    private void HandleMovement()
+    {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
@@ -23,7 +64,7 @@ public class PlayerController : MonoBehaviour
 
         if (!canMove)
         {
-            Vector3 moveDirX = new Vector3(moveDir.x,0,0).normalized;
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
             canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
             if (canMove)
             {
@@ -31,7 +72,7 @@ public class PlayerController : MonoBehaviour
             }
             else
             {
-                Vector3 moveDirZ = new Vector3(0,0,moveDir.z).normalized;
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
                 canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
                 if (canMove)
                 {
@@ -58,9 +99,25 @@ public class PlayerController : MonoBehaviour
 
     }
 
-    public bool IsWalking()
+    private void HandleInteractions()
     {
-        return isWalking;
-    }
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        if (moveDir != Vector3.zero)
+        {
+            lastInteractDir = moveDir;
+        }
+
+        float interactDistance = 2f;
+        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask))
+        {
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+
+            }
+        }
+
+    }
 }
